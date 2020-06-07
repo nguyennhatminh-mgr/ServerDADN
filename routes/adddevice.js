fe72db9124c7c4c680862115805f9ce22d93dc0f
Lam <lamkhmt07@gmail.com>
2020-06-07 20:01:40 +0700

    complete addDivice Api

--- routes/adddevice.js
+++ routes/adddevice.js
@@ -4,11 +4,37 @@ var router = express.Router();
 //  variable connect is used to connect to database
 var connect = require('../connect/connect');
 
-router.get("/adddevice",(req,res,next) => {
-    // Your code here
+router.post("/addDevice", (req,res) => {
+    var valueInsert = [req.body.id, req.body.type ,req.body.id_room];
 
-    res.send("adddevice");
+    query = "INSERT INTO Device(id, type, id_room) VALUES (?);";
+    console.log(req.body)
+    connect.getConnection((err,connection) => {
+        if(err) {
+            res.send("fail");
+        };
+        connection.query(query, [valueInsert] ,(err,rows) => {
+            //connection.release();
+            if(err) res.send("fail");
+            else res.send("ok");
+        });
+      });
 }); 
-// Your code here
+
+router.post("/addRoom", (req, res)=>{
+    
+    var valueInsert = [req.body.id, req.body.name];
+
+    query = "INSERT INTO Room(id,name) VALUES (?);";
+
+    connect.getConnection((err,connection) => {
+        if(err) res.send("fail");
+        connection.query(query, [valueInsert] ,(err,rows) => {
+            //connection.release();
+            if(err) res.send(err);
+            res.send("ok");
+        });
+      });
+});
 
 module.exports = router;

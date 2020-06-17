35f1366008aef015b79ad89df6f46d8ae17b45b7
lucle99 <48874410+lucle99@users.noreply.github.com>
2020-06-17 14:17:17 +0700

    Update viewroom.js

--- routes/viewroom.js
+++ routes/viewroom.js
@@ -9,14 +9,14 @@ router.get('/viewroom/:id',(req,res,next) => {
     const user_id = req.params.id;
     let query = "";
     if(user_id === ID_ADMIN){
-        query  = `SELECT m.id as userID, m.realname, r.name, d.id, v.value, max(v.received_time) as received_time from myuser m 
+        query  = `SELECT m.id as userID, m.realname, r.name, d.id, v.value, v.received_time as received_time from myuser m 
         join adminroom ad on m.id = ad.id_user join room r on r.id = ad.id_room join device d on d.id_room = ad.id_room 
-        join valueofdevice v on v.id_device = d.id group by v.id_device`;
+        join valueofdevice v on v.id_device = d.id and v.received_time = (SELECT max(received_time) from valueofdevice)  group by v.id_device`;
     }
     else{
         query = `SELECT m.id as userID, m.realname, r.name, d.id, v.value, max(v.received_time) as received_time from myuser m 
         join adminroom ad on m.id = ad.id_user join room r on r.id = ad.id_room join device d on d.id_room = ad.id_room 
-        join valueofdevice v on v.id_device = d.id where m.id = "${user_id}"`;
+        join valueofdevice v on v.id_device = d.id where v.received_time = (SELECT max(received_time) from valueofdevice) and m.id = "${user_id}"`;
     }
     // Your code here
     connect.getConnection((err, con)=>{

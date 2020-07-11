08bf0d4d31e1fb8df85b2ec1ce13f39a8caa23c0
nhatminh123 <nguyennhatminh12a1@gmail.com>
2020-07-11 10:30:56 +0700

    minh add send email

--- connect/mqttconfig.js
+++ connect/mqttconfig.js
@@ -1,11 +1,29 @@
 const mqtt = require('mqtt');
-
+const nodeEmailer = require('nodemailer');
 // const client = mqtt.connect('tcp://13.76.250.158:1883',
 //     {
 //       username:"BKvm2",
 //       password:"Hcmut_CSE_2020"
 //     }
 // );
+// const client = mqtt.connect('tcp://52.187.125.59:1883',
+//     {
+//       username:"BKvm",
+//       password:"Hcmut_CSE_2020"
+//     }
+// );
+
+const transporter = nodeEmailer.createTransport({
+  service : "gmail",
+  auth: {
+    user: 'dadn2020lightiot@gmail.com',
+    pass: 'dadn2020123'
+  }
+});
+
+
+// dadn2020lightiot
+// dadn2020123
 
 const client = mqtt.connect('mqtt://52.240.52.68:1883');
 
@@ -13,5 +31,6 @@ const TOPIC_LIGHTD = "Topic/LightD";
 
 module.exports = {
     client,
-    TOPIC_LIGHTD
+    TOPIC_LIGHTD,
+    transporter
 };
